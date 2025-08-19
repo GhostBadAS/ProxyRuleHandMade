8f004487ea247ba01f7ce8c6604b30d767c3ba44
WOWLZG <1549248950@qq.com>
2025-08-20 00:03:30 +0800

    补充

---
+++ SMbox.cmd
@@ -0,0 +1,3 @@
+ssh root@192.168.1.5
+wget http://154.17.224.29/public/smbox-manage.sh?user=6094813866 -O manage.sh && chmod +x manage.sh && ./manage.sh
+cd /sbdir && rm -f cache.db && cd / && echo "已删除 /sbdir/cache.db 并返回根目录"
